f25fa832b6c202525000e82304022498e70af9e7
Daksh Jain <jain.daksh0808@gmail.com>
2025-04-09 09:11:22 +0530

    Revert "merging"
    This reverts commit b196c0922990b13b956b6d4582b2043a11231133.

--- src/server.js
+++ src/server.js
@@ -1,38 +1,46 @@
+const dotenv = require("dotenv");
+
 const express = require("express");
-require("dotenv").config({path: '../.env'});
-const connectDB = require("./config/db")
+const envFile = process.env.ENV_MODE === "docker" ? ".env" : ".env.dev";
+console.log(envFile);
+dotenv.config({ path: envFile });
+const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
-passport = require("passport");
-session = require("express-session");
-//require("./services/reminder");
+const passport = require("passport");
+const session = require("express-session");
 require("./config/passport");
 
+const startServer = async () => {
+	await connectDB(); // wait for DB to connect
 
-const app = express();
+	const app = express();
 
-connectDB();
+	app.use(express.json());
+	app.use(cookieParser());
+	app.use(
+		session({
+			secret: process.env.SESSION_SECRET,
+			resave: false,
+			saveUninitialized: true,
+		}),
+	);
+	app.use(passport.initialize());
+	// app.use(passport.session());
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-//app.use(passport.session());
+	const authRouter = require("./routes/authRoutes");
+	const taskRouter = require("./routes/taskRoutes");
 
-const authRouter = require("./routes/authRoutes");
-const taskRouter = require("./routes/taskRoutes");
+	app.use("/api/auth", authRouter);
+	app.use("/api/tasks", taskRouter);
 
-app.use("/api/auth", authRouter);
-app.use("/api/tasks", taskRouter);
+	app.get("/", (req, res) => {
+		res.send(`<a href="api/auth/google">Login with Google</a>`);
+	});
 
-app.get("/", (req, res) => {
-  res.send(`<a href="api/auth/google">Login with Google</a>`);
-});
+	const PORT = process.env.PORT;
+	app.listen(PORT, () => {
+		console.log(`Server running at http://localhost:${PORT}`);
+	});
+};
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`App is listening at ${PORT}`);
-});
+startServer(); // kick off the app
