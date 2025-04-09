7ab0a1336494ba43da70dfcf61ae6749f0006824
Daksh Jain <jain.daksh0808@gmail.com>
2025-04-09 08:56:25 +0530

    Remove .env file from tracking

--- src/server.js
+++ src/server.js
@@ -1,27 +1,20 @@
+const dotenv = require("dotenv");
+
 const express = require("express");
-<<<<<<< HEAD
-const envFile = process.env.ENV_MODE === "docker" ? ".env" : ".env1";
+const envFile = process.env.ENV_MODE === "docker" ? ".env" : ".env.dev";
 console.log(envFile);
 dotenv.config({ path: envFile });
 const connectDB = require("./config/db");
-=======
-require("dotenv").config({path: '../.env'});
-const connectDB = require("./config/db")
->>>>>>> parent of 282dc8e (added mailing service and verifying email at registration)
 const cookieParser = require("cookie-parser");
-passport = require("passport");
-session = require("express-session");
-//require("./services/reminder");
+const passport = require("passport");
+const session = require("express-session");
 require("./config/passport");
 
+const startServer = async () => {
+	await connectDB(); // wait for DB to connect
 
-const app = express();
+	const app = express();
 
-connectDB();
-
-<<<<<<< HEAD
-	const authRouter = require("./routes/authRoutes");
-	const taskRouter = require("./routes/taskRoutes");
 	app.use(express.json());
 	app.use(cookieParser());
 	app.use(
@@ -32,30 +25,22 @@ connectDB();
 		}),
 	);
 	app.use(passport.initialize());
-	app.use(passport.session());
-=======
-app.use(express.json());
-app.use(cookieParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-//app.use(passport.session());
->>>>>>> parent of 282dc8e (added mailing service and verifying email at registration)
+	// app.use(passport.session());
 
-const authRouter = require("./routes/authRoutes");
-const taskRouter = require("./routes/taskRoutes");
+	const authRouter = require("./routes/authRoutes");
+	const taskRouter = require("./routes/taskRoutes");
 
-app.use("/api/auth", authRouter);
-app.use("/api/tasks", taskRouter);
+	app.use("/api/auth", authRouter);
+	app.use("/api/tasks", taskRouter);
 
-app.get("/", (req, res) => {
-  res.send(`<a href="api/auth/google">Login with Google</a>`);
-});
+	app.get("/", (req, res) => {
+		res.send(`<a href="api/auth/google">Login with Google</a>`);
+	});
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`App is listening at ${PORT}`);
-});
+	const PORT = process.env.PORT;
+	app.listen(PORT, () => {
+		console.log(`Server running at http://localhost:${PORT}`);
+	});
+};
+
+startServer(); // kick off the app
